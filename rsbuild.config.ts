36dd69194386a2f273de0bd129ad8906e4b9e145
utrk <155697796+utrk@users.noreply.github.com>
2025-10-26 18:49:50 +0900

    Configuration and change for deploying to GitHub Pages

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,6 +3,9 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    assetPrefix: "/float-increment/",
+  },
   html: {
     title: "Increment/decrement floating-point number",
     tags: [
