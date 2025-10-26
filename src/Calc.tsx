1077fc1834b70983f313bced9be861fd1cd27749
utrk <155697796+utrk@users.noreply.github.com>
2025-10-27 07:02:33 +0900

    Add a field for binary value

--- src/Calc.tsx
+++ src/Calc.tsx
@@ -6,13 +6,16 @@ const float64Buffer = new ArrayBuffer(8);
 const float64View = new DataView(float64Buffer);
 
 const Calc = () => {
-  const [value, setValue] = useState("");
+  const [literalValue, setLiteralValue] = useState("");
+  const [binaryValue, setBinaryValue] = useState("");
 
-  const handleBlur: React.FocusEventHandler<HTMLInputElement> = () => {
-    const number = Number(value);
+  const handleLiteralFieldBlur: React.FocusEventHandler<HTMLInputElement> = () => {
+    const number = Number(literalValue);
     if (!Number.isNaN(number) || !Number.isNaN(float64View.getFloat64(0))) {
       float64View.setFloat64(0, number);
     }
+    setLiteralValue(number.toString());
+    setBinaryValue(float64View.getBigUint64(0).toString(2));
   };
 
   const incrementFloat64 = () => {
@@ -21,15 +24,18 @@ const Calc = () => {
 
     float64View.setBigUint64(0, bigUint64);
 
-    setValue(float64View.getFloat64(0).toString());
+    setLiteralValue(float64View.getFloat64(0).toString());
+    setBinaryValue(float64View.getBigUint64(0).toString(2));
   };
+
   const decrementFloat64 = () => {
     let bigUint64 = float64View.getBigUint64(0);
     bigUint64 = bigUint64 === 0n ? 0xffffffffffffffffn : bigUint64 - 1n;
 
     float64View.setBigUint64(0, bigUint64);
 
-    setValue(float64View.getFloat64(0).toString());
+    setLiteralValue(float64View.getFloat64(0).toString());
+    setBinaryValue(float64View.getBigUint64(0).toString(2));
   };
 
   const handleClickIncrement = () => {
@@ -44,18 +50,21 @@ const Calc = () => {
       <TextField
         fullWidth
         label="Number (Numeric Literal)"
-        variant="filled"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-        onBlur={handleBlur}
+        variant="outlined"
+        value={literalValue}
+        onChange={(event) => setLiteralValue(event.target.value)}
+        onBlur={handleLiteralFieldBlur}
         sx={{ mt: 1 }}
       />
-      <Stack
-        direction="row"
-        spacing={1}
-        useFlexGap
-        sx={{ flexWrap: "wrap", mt: 1 }}
-      >
+      <TextField
+        fullWidth
+        label="Binary value"
+        variant="filled"
+        value={binaryValue}
+        disabled
+        sx={{ mt: 1 }}
+      />
+      <Stack direction="row" spacing={1} useFlexGap sx={{ flexWrap: "wrap", mt: 1 }}>
         <Button onClick={handleClickIncrement} variant="contained">
           Increment
         </Button>
